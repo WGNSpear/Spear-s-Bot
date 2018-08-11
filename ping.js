0772eacb1ca0aa3237f5a50e3baabf084bbd9d41
WGNSpear <40544275+WGNSpear@users.noreply.github.com>
2018-08-11 18:44:56 +0100

    Update ping.js

--- ping.js
+++ ping.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 
   message.channel.send("Checking ping, please wait.").then(function(m) {
-    m.edit(`The current ping of this server is: ${m.createdTimestamp - message.createdTimestamp - 40} ms`)
+    m.edit(`The current ping of this server is: ${m.createdTimestamp - message.createdTimestamp - 10} ms`)
   })
   return;
 
