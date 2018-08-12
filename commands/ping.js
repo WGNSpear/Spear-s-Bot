986f657641b1ab6f5b44d141bde53b1ba5ba7d5a
WGNSpear <40544275+WGNSpear@users.noreply.github.com>
2018-08-12 22:20:32 +0100

    Make Ping Embed

--- commands/ping.js
+++ commands/ping.js
@@ -2,12 +2,22 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-  message.channel.send("Checking ping, please wait.").then(function(m) {
-    m.edit(`The current ping of this server is: ${m.createdTimestamp - message.createdTimestamp - 40} ms`)
-  })
-  return;
+    let pingembed = new Discord.RichEmbed()
+    .setTitle("Checking Ping... Please Wait")
+    .setDescription(" ")
+    .setColor("#0000ff")
+
+    let finalpingembed = new Discord.RichEmbed()
+    .setTitle("Done!")
+    .setDescription(`The current ping of this server is: ${bot.ping.toLocaleString() -23} ms`)
+    .setColor("#0000ff")
+
+    message.channel.send(pingembed).then(function(m) {
+    m.edit(finalpingembed)
+  });
 
 }
+// bot.ping.toLocaleString()
 
 module.exports.help = {
   name: "ping"
