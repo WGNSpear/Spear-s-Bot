fc4b9997c91436621de90171a97f4dc64c5379a1
WGNSpear <40544275+WGNSpear@users.noreply.github.com>
2018-08-12 23:40:33 +0100

    Dev Only DM

--- commands/DM.js
+++ commands/DM.js
@@ -3,17 +3,17 @@ const Discord =require("discord.js");
 module.exports.run = async (bot, message, args) => {
 
   //let Dev = message.author.id === 370606740523188227
-  let DMUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-  if(!DMUser) return message.channel.send("Only the developer can do that!");
-  let DMMessage = args.join(" ").slice(22);
-  if(message.author.id = (370606740523188227))
-    (DMUser).send(DMMessage)
-    message.delete();
-
+  if(message.author.id != (370606740523188227)) return message.chanel.send("You are not a Dev!");
+    let DMUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    if(!DMUser) return message.channel.send("Cannot find user!");
+    let DMMessage = args.join(" ").slice(22);
+    if(message.author.id = (370606740523188227))
+      (DMUser).send(DMMessage)
+      message.delete();
 
 //  (DMUser).send(DMMessage)
 //  message.delete()
-
+//  multiple Devs = if (message.author.id === "ID here" && message.author.id === "ID here")
 
 }
 
